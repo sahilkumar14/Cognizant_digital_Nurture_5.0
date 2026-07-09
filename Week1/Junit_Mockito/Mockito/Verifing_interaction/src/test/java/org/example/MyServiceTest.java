0b01346a5d188373c13975c733eb717416bbe0ea
sahilkumar <sahilkv14@gmail.com>
2026-07-09 09:28:51 +0530

    Added mockito exercise

---
+++ Week1/Junit_Mockito/Mockito/Verifing_interaction/src/test/java/org/example/MyServiceTest.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.junit.Test;
+
+import static org.mockito.Mockito.*;
+
+public class MyServiceTest {
+
+    @Test
+    public void testVerifyInteraction(){
+        ExternalApi mockApi = mock(ExternalApi.class);
+
+        MyService service = new MyService(mockApi);
+
+        service.fetchData();
+
+        verify(mockApi).getData();
+    }
+}
